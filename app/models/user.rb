0f63c7a0a99678629c5f73d1c7b99967786579af
MattGomb <gombosonline@gmail.com>
2023-02-27 14:57:56 -0500

    Add model connections and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :expenses, foreign_key: 'author_id', dependent: :destroy
+  has_many :groups, foreign_key: 'author_id', dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :email, presence: true, length: { minimum: 1, maximum: 50 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
 end
