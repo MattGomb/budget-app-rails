4d218b8ce867cada5dda61fc2b3c5442f9359763
MattGomb <gombosonline@gmail.com>
2023-02-27 13:21:03 -0500

    Add models and databases

--- db/migrate/20230227180938_create_expenses.rb
+++ db/migrate/20230227180938_create_expenses.rb
@@ -1,7 +1,7 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.string :name
       t.decimal :amount
 
