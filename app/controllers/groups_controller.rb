61914f50047d11f3861cd7e66eae98d713ce2efb
MattGomb <gombosonline@gmail.com>
2023-02-28 16:44:24 -0500

    Add styling and logic

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to category_url(@group), notice: 'Group was successfully created.' }
+        format.html { redirect_to category_url(@group), notice: 'Category was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
